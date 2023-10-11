905638134f5e64a919d39fb58d78f934c77efb0a
skiflok <anisimov.a.v@inbox.ru>
2023-10-11 16:49:19 +0300

    init server

---
+++ BankBranch/src/main/java/com/example/BankBranch/BankBranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankBranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBranchApplication.class, args);
+	}
+
+}
